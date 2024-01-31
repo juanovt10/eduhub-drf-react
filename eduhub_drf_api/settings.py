da14216253056c0b053cff0517d486ed45c5a1a1
Juan Velasquez <juanovelasquez10@gmail.com>
2024-01-31 17:36:42 +0000

    added comma, to separate allowed hosts list

--- eduhub_drf_api/settings.py
+++ eduhub_drf_api/settings.py
@@ -63,7 +63,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOST')
+    os.environ.get('ALLOWED_HOST'),
     '8000-juanovt10-eduhubdrfreac-9gnsdlmtz9i.ws-eu107.gitpod.io',
 ]
  
