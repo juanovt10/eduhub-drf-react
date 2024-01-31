1192ef73fa8c1f201a4e3b6d1e7dd3b37af3051e
Juan Velasquez <juanovelasquez10@gmail.com>
2024-01-31 17:30:58 +0000

    Add heroku host for deployment

--- eduhub_drf_api/settings.py
+++ eduhub_drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -62,8 +63,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST')
     '8000-juanovt10-eduhubdrfreac-9gnsdlmtz9i.ws-eu107.gitpod.io',
-    'https://eduhub-drf-api-8e84adf897cc.herokuapp.com',
 ]
  
 CSRF_TRUSTED_ORIGINS = ['https://8000-juanovt10-eduhubdrfreac-9gnsdlmtz9i.ws-eu107.gitpod.io',]
@@ -114,9 +115,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
-    CORS_ALLOWED_ORIGINS_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
