87eac28dcd9d51462eb2e7f0d52e13a1b21be427
Juan Velasquez <juanovelasquez10@gmail.com>
2024-03-01 13:31:35 +0000

    Amend wish list detail view url pattern

--- wish_lists/urls.py
+++ wish_lists/urls.py
@@ -3,5 +3,5 @@ from wish_lists import views
 
 urlpatterns = [
     path('wish_lists/', views.WishListList.as_view()),
-    path('enrollments/<int:pk>/', views.WishListDetail.as_view()),
+    path('wish_lists/<int:pk>/', views.WishListDetail.as_view()),
 ]
