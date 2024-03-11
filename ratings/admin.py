21152eeface421c27be250e634abcbb6764afe94
Juan Velasquez <juanovelasquez10@gmail.com>
2024-03-11 11:39:56 +0000

    Add admin view for ratings

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Rating
 
-# Register your models here.
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'owner', 'created_at')
+    search_fields = ['title', 'owner', 'course']
+    list_filter = ('title', 'owner', 'course')
