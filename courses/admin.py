c914632ae379b35327e7eb66205965c4674db019
Juan Velasquez <juanovelasquez10@gmail.com>
2024-03-11 11:36:39 +0000

    Add delete method to ProfileDetail view to delete the User model when the Profile model is deleted

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'owner', 'created_at')
+    search_fields = ['title', 'owner', 'category']
+    list_filter = ('category', 'owner')
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,8 @@
 from django.db.models import Count
-from rest_framework import generics, filters
+from rest_framework import generics, filters, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -38,3 +41,10 @@ class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
         enrollments_count = Count('owner__enrollment', distinct=True),
         wish_list_count = Count('owner__wishlist', distinct=True),
     ).order_by('-created_at')
+
+    def delete(self, request, *args, **kwargs):
+        profile = self.get_object()
+        user = get_object_or_404(User, id=profile.owner.id)
+
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
