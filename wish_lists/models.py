d6c47f3255fbf556fb0130fb61a5b9edbe6dcbd9
Juan Velasquez <juanovelasquez10@gmail.com>
2024-03-29 21:51:10 +0000

    Add related name to owner in wish list model

--- wish_lists/models.py
+++ wish_lists/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from courses.models import Course
 
 class WishList(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wishlist')
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     added_at = models.DateTimeField(auto_now_add=True)
 
