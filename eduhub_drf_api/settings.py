8dd7a3932d2c76dbab7e48d0218c374de1801522
Juan Velasquez <juanovelasquez10@gmail.com>
2024-03-29 08:42:23 +0000

    Update secret key to restart API

--- eduhub_drf_api/settings.py
+++ eduhub_drf_api/settings.py
@@ -59,8 +59,6 @@ REST_AUTH_SERIALIZERS = {
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
